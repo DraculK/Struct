c92ee073aacdd7b651b010281f2e83aba426103c
DraculK <kayranvieira456@hotmail.com>
2020-05-04 18:55:33 -0300

    Update 3.rb

--- Struct Lista 1/3.rb
+++ Struct Lista 1/3.rb
@@ -1,1 +1,14 @@
 #Exercício - 3
+a = 10
+b = 10.5
+c = "trainee"
+d = [5,"oi",10]
+e = :symbol
+f = { :nome => "Pedro", :idade => 28 }
+
+puts a.class
+puts b.class
+puts c.class
+puts d.class
+puts e.class
+puts f.class
