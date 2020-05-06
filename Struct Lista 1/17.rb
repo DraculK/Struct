bbbdd1c12cb1fe93ff12520b6188beeffb2207ff
DraculK <kayranvieira456@hotmail.com>
2020-05-06 17:03:19 -0300

    Exercícios

--- Struct Lista 1/17.rb
+++ Struct Lista 1/17.rb
@@ -1,4 +1,23 @@
 #Exercício - 17
 puts "Digite o valor: "
 valor = gets.to_i
-n=0
+n100 = valor/100
+valor-=n100*100
+n50 = valor/50
+valor-=n50*50
+n20 = valor/20
+valor-=n20*20
+n10 = valor/10
+valor-=n10*10
+n5 = valor/5
+valor-=n5*5
+n2 = valor/2
+valor-=n2*2
+
+puts "#{n100} nota(s) de 100"
+puts "#{n50} nota(s) de 50"
+puts "#{n20} nota(s) de 20"
+puts "#{n10} nota(s) de 10"
+puts "#{n5} nota(s) de 5"
+puts "#{n2} nota(s) de 2"
+puts "Resto: #{valor}" 
---
+++ Struct Lista 1/desafio.rb
@@ -0,0 +1,2 @@
+#Desafio - 1
+pratos = {:entrada=>["Bruschetta","Salada","Sopa"], :principal=>["Bobó de camarão","Lasanha","Almôndega"], :sobremesa=>["Torta de limão", "Sorvete", "Mousse de maracujá"]}
