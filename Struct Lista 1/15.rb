8e7de38d8d3a3b7cde35fbcbd5d59c2dbfec4a90
DraculK <kayranvieira456@hotmail.com>
2020-05-06 09:24:00 -0300

    Exercícios

--- Struct Lista 1/15.rb
+++ Struct Lista 1/15.rb
@@ -1,1 +1,11 @@
 #Exercício - 15
+dados=Hash.new()
+puts "Digite o nome do aluno: "
+dados[:nome] = gets.chomp
+puts "Digite a idade do aluno: "
+dados[:idade] = gets.chomp
+puts "Digite a matrícula do aluno: "
+dados[:matricula] = gets.chomp
+puts "Digite o email do aluno: "
+dados[:email] = gets.chomp
+puts dados.each_value {|value| puts value}
