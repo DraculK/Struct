c95f46edc59f74403b08e6547e8c54fcb7427216
DraculK <kayranvieira456@hotmail.com>
2020-05-06 09:08:19 -0300

    Exercícios

--- Struct Lista 1/12.rb
+++ Struct Lista 1/12.rb
@@ -1,1 +1,30 @@
 #Exercício - 12
+def bissexto(num)
+    if((num%4==0) && (num%100!=0))
+        return 1
+    elsif(num%400==0)
+        return 1
+    elsif(num%4!=0)
+        #eturn 0
+    end
+end
+
+anos = (1901..2000).to_a
+leapyear=[]
+i=0
+anos.each do |anos|
+    if(bissexto(anos)==1)
+        leapyear[i]=anos
+        #puts leapyear[i]
+        i+=1
+    end
+end
+
+leapyear.each do |leapyear|
+    print "#{leapyear} "
+end
+print "\n"
+
+
+
+
---
+++ Struct Lista 1/14.rb
@@ -0,0 +1,3 @@
+#Exercício - 14
+aluno1 = {"nome" => "Pedro", "idade" => 20, "altura" =>1.72}
+puts aluno1
