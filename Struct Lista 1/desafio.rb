aaaed84bbe050f0c3368025dbaa403953c856c07
DraculK <kayranvieira456@hotmail.com>
2020-05-06 18:35:53 -0300

    Exercícios

--- Struct Lista 1/desafio.rb
+++ Struct Lista 1/desafio.rb
@@ -1,2 +1,4 @@
 #Desafio - 1
-pratos = {:entrada=>["Bruschetta","Salada","Sopa"], :principal=>["Bobó de camarão","Lasanha","Almôndega"], :sobremesa=>["Torta de limão", "Sorvete", "Mousse de maracujá"]}
+pratos = {"entrada"=>["Bruschetta","Salada","Sopa"], "principal"=>["Bobó de camarão","Lasanha","Almôndega"], "sobremesa"=>["Torta de limão", "Sorvete", "Mousse de maracujá"]}
+print "[#{pratos["entrada"].sample}, #{pratos["principal"].sample}, #{pratos["sobremesa"].sample}]"
+print "\n"
