ae594a07e3a948b6a03c3d5f67677dfb87d5a3e0
DraculK <kayranvieira456@hotmail.com>
2020-05-07 20:03:08 -0300

    Exercícios

--- Struct Lista 1/desafio2.rb
+++ Struct Lista 1/desafio2.rb
@@ -1,4 +1,18 @@
 #Desafio - 2
 pratos = {"entrada"=>["Bruschetta"=>2.5,"Salada"=>3,"Sopa"=>5], "principal"=>["Bobó de camarão"=>30,"Lasanha"=>15,"Almôndega"=>24], "sobremesa"=>["Torta de limão"=>12, "Sorvete"=>6, "Mousse de maracujá"=>20]}
-print "[#{pratos["entrada"].sample}, #{pratos["principal"].sample}, #{pratos["sobremesa"].sample}]"
-print "\n"
+escolha = []
+total=0
+pratos.each do |key,value|
+    if key=="entrada"
+        if value==value[0]["Bruschetta"]
+                total=value[0]["Bruschetta"]
+        elsif value==value[0]["Salada"]
+                total=value[0]["Salada"]
+        elsif value==value[0]["Sopa"]
+                total=value[0]["Sopa"]
+        end
+    end
+end
+
+puts total
+
