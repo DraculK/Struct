839de0e2670fbb83760327a3b6b875d0ec4c26cc
DraculK <kayranvieira456@hotmail.com>
2020-05-07 18:53:57 -0300

    Desafios

--- Struct Lista 1/desafio.rb
+++ Struct Lista 1/desafio.rb
@@ -1,4 +1,16 @@
 #Desafio - 1
 pratos = {"entrada"=>["Bruschetta","Salada","Sopa"], "principal"=>["Bobó de camarão","Lasanha","Almôndega"], "sobremesa"=>["Torta de limão", "Sorvete", "Mousse de maracujá"]}
-print "[#{pratos["entrada"].sample}, #{pratos["principal"].sample}, #{pratos["sobremesa"].sample}]"
+#print "[#{pratos["entrada"].sample}, #{pratos["principal"].sample}, #{pratos["sobremesa"].sample}]"
+#print "\n"
+escolha =[]
+pratos.each do |key,value|
+    if key=="entrada"
+        escolha[0] = value.sample
+    elsif key=="principal"
+        escolha[1] = value.sample
+    elsif key=="sobremesa"
+        escolha[2] = value.sample
+    end
+end
+print escolha
 print "\n"
---
+++ Struct Lista 1/desafio2.rb
@@ -0,0 +1,4 @@
+#Desafio - 2
+pratos = {"entrada"=>["Bruschetta"=>2.5,"Salada"=>3,"Sopa"=>5], "principal"=>["Bobó de camarão"=>30,"Lasanha"=>15,"Almôndega"=>24], "sobremesa"=>["Torta de limão"=>12, "Sorvete"=>6, "Mousse de maracujá"=>20]}
+print "[#{pratos["entrada"].sample}, #{pratos["principal"].sample}, #{pratos["sobremesa"].sample}]"
+print "\n"
