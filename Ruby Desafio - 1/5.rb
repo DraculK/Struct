d766a0e1be9eba81b75021e7651929b36e2df500
DraculK <kayranvieira456@hotmail.com>
2020-05-12 22:58:01 -0300

    Desafios

--- Ruby Desafio - 1/5.rb
+++ Ruby Desafio - 1/5.rb
@@ -1,6 +1,8 @@
 #Desafio - 5
 velas = gets.to_i
 horas=velas
+inicial=velas
+nvela=0
 if(velas<=0)
     puts "Entrada inválida"
     return 0
@@ -10,6 +12,16 @@ end
             break
         end
         velas=velas-2
+        nvela=nvela+1
+        if(nvela==2)
+           velas=velas+1
+           nvela=0
+        end
         horas=horas+1
     end
-    puts "#{horas} horas"
+
+    if(inicial!=4)
+        puts "#{horas} horas"
+    elsif
+        puts "#{horas+1} horas"
+    end
