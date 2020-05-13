8bf43427783bd37556664cd948f9c8b9dcab5d2a
DraculK <kayranvieira456@hotmail.com>
2020-05-12 22:19:39 -0300

    Desafios

---
+++ Ruby Desafio - 1/6.rb
@@ -0,0 +1,20 @@
+#Desafio - 6
+valor=gets.to_f
+rendimento=gets.to_f
+conta=gets.to_i
+prazo=gets.to_i
+prazo1=prazo
+inicial=valor
+if(valor==0 || rendimento==0 || conta==0 || prazo==0)
+    puts "Entrada inválida"
+elsif
+    while(prazo!=0)
+        if(valor-inicial>=conta)
+            break
+        end
+        conta=conta+conta*0.01
+        valor=valor+valor*rendimento
+        prazo=prazo-1
+    end
+end
+puts "Wallif pagou a conta em #{prazo1-prazo} meses"
