69492ba39c1c627e829f5c76753af573fb9c3349
DraculK <kayranvieira456@hotmail.com>
2020-05-20 19:43:57 -0300

    Update 3.rb

--- Ruby POO/3.rb
+++ Ruby POO/3.rb
@@ -69,7 +69,25 @@ class ContaC < Pessoa
     end
 
     attr_reader :saldo, :numero, :senha
-
+    def Saldo
+        puts "Seu saldo atual é: #{saldo}"
+    end
+    def ModificarTel
+        puts "Digite o novo número de telefone: "
+        novotelefone = gets.to_s
+        @telefone=novotelefone
+        puts "Seu novo número de telefone é: #{@telefone}"
+    end
+    def MostrarPessoal(nome,cpf,telefone,idade)
+        puts "Nome: #{nome}"
+        puts "CPF: #{cpf}"
+        puts "Telefone: #{telefone}"
+        puts "Idade: #{idade}"
+    end
+    def MostrarConta(numeroconta,senha)
+        puts "Número da conta: #{numeroconta}"
+        puts "Senha: #{senha}"
+    end
     def Depositar(num)
         @saldo = @saldo+num
         puts "Você conseguiu depositar: #{num}"
@@ -86,7 +104,7 @@ class ContaC < Pessoa
 end
 
 contaEx = ContaP.new("Ramón Valdés", "000.000.000-00", "4002-8922", 65, 100000, "1234 013 12345678-9", "4321")
-contaEX1 = ContaC.new("Hayao Miyazaki", "111.111.111-11", "9519-9519", 79, 60000, "1234 013 12345678-0", "1234")
+# contaEX1 = ContaC.new("Hayao Miyazaki", "111.111.111-11", "9519-9519", 79, 60000, "1234 013 12345678-0", "1234")
 # contaEx1 = ContaC.new("Ramón Valdés", "000.000.000-00", "4002-8922", 65, 100000, "1234 013 12345678-9", "4321")
 # puts contaEx.nome
 # puts contaEx.cpf
@@ -261,7 +279,116 @@ while(option!=6)
         end
         if(tipo==1)
             puts "Digite o CPF da conta que você quer acessar: "
-            cpff=gets.to_s
+            cpff=gets.to_s.strip
+            for d in 0..i+1 
+                if cpff==cpfcorrente[d]
+                    puts "Digite a sua opção: "
+                    puts "1. Depositar"
+                    puts "2. Sacar"
+                    puts "3. Exibir saldo"
+                    puts "4. Modificar telefone"
+                    puts "5. Exibir dados pessoais"
+                    puts "6. Exibir dados da conta"
+                    puts "7. Sair"
+                    option = gets.to_i
+                    while(option<1 || option>7)
+                        puts "Escolha uma operação válida"
+                        puts "1. Depositar"
+                        puts "2. Sacar"
+                        puts "3. Exibir saldo"
+                        puts "4. Modificar telefone"
+                        puts "5. Exibir dados pessoais"
+                        puts "6. Exibir dados da conta"
+                        puts "7. Sair"
+                        option = gets.to_i
+                    end
+                    while(option!=7)
+                        if(option==1)
+                            puts
+                            puts "Quando deseja depositar?"
+                            num = gets.to_i
+                            cc.Depositar(num)
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==2)
+                            puts
+                            puts "Quando deseja sacar?"
+                            num = gets.to_i
+                            cc.Sacar(num)
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==3)
+                            puts
+                            cc.Saldo
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==4)
+                            puts
+                            cc.ModificarTel
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==5)
+                            puts
+                            cc.MostrarPessoal(cc.nome,cc.cpf,cc.telefone,cc.idade)
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==6)
+                            puts
+                            cc.MostrarConta(cc.numero,cc.senha)
+                            puts
+                            puts "1. Depositar"
+                            puts "2. Sacar"
+                            puts "3. Exibir saldo"
+                            puts "4. Modificar telefone"
+                            puts "5. Exibir dados pessoais"
+                            puts "6. Exibir dados da conta"
+                            puts "7. Sair"
+                            option = gets.to_i
+                        elsif(option==7)
+                            return 0
+                        end
+                    end
+                elsif(d>i && option!=7)
+                    puts "Não há conta com esse CPF"
+                    break;
+                end
+                d+=1
+            end
         elsif(tipo==2)
             puts "Digite o CPF da conta que você quer acessar: "
             cpff=gets.to_s.strip
@@ -364,6 +491,7 @@ while(option!=6)
                         puts "7. Sair"
                         option = gets.to_i
                     elsif(option==7)
+                        break
                         return 0
                     end
                 end
@@ -471,7 +599,7 @@ while(option!=6)
                                 return 0
                             end
                         end
-                    elsif(d>f)
+                    elsif(d>f && option!=7)
                         puts "Não há conta com esse CPF"
                         break;
                     end
