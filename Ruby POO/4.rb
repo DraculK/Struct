5f16e1450d92b76a0be7edf373e398b1765b55dd
DraculK <kayranvieira456@hotmail.com>
2020-05-21 17:12:21 -0300

    Update 4.rb

--- Ruby POO/4.rb
+++ Ruby POO/4.rb
@@ -20,6 +20,7 @@ while qtdTurmas<1 || qtdTurmas>10
     qtdTurmas = gets.to_i
 end
 turmas = []
+alunos = []
 materias = ["Cálculo 1", "Cálculo 2", "Cálculo 3", "Física 1", "Física 2", "Estruturas de Dados", "Algoritmos e Programação de Computadores", "Técnicas de Programação 1", "Sistemas Digitais", "Eletromagnetismo"]
 qtdTurmas.times do 
     turmas<<Turma.new()
@@ -29,4 +30,8 @@ while i!=qtdTurmas
     turmas[i].materia=materias[i]
     i+=1
 end
- 
+x=0
+while x!=qtdTurmas
+    alunos[x]=rand(5..20)
+    x+=1
+end
