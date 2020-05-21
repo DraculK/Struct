77e8ca2568065ad291cab6695623fd74ae255c61
DraculK <kayranvieira456@hotmail.com>
2020-05-21 15:42:19 -0300

    Exercícios

--- Ruby POO/4.rb
+++ Ruby POO/4.rb
@@ -1,1 +1,27 @@
 #Exercício - 4
+class Turma
+    attr_accessor :materia, :alunos
+    def initialize
+        @alunos = []
+    end
+    aprovados=0
+    def aprovado(nota)
+        if nota>=5
+            aprovados = 1
+        elsif nota<5
+            aprovados = 0
+        end
+    end
+end
+puts "Qual o número de turmas desse semestre?"
+qtdTurmas = gets.to_i
+while qtdTurmas<1 || qtdTurmas>10
+    puts "Quantidade inválida, tente outro valor entre 1 e 10"
+    qtdTurmas = gets.to_i
+end
+turmas = []
+materias = ["Cálculo 1", "Cálculo 2", "Cálculo 3", "Física 1", "Física 2", "Estruturas de Dados", "Algoritmos e Programação de Computadores", "Técnicas de Programação 1", "Sistemas Digitais", "Eletromagnetismo"]
+
+qtdTurmas.times do 
+    turmas<<Turma.new()
+end
